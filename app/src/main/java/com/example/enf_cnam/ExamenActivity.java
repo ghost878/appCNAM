b7b6768acaab920cf1cedcbb8e540dde7b7e284e
Hippolyte Massicot <hippolyte.massicot@gmail.com>
2021-02-02 17:48:11 +0100

    requête vers api, affichage reste à faire

--- app/src/main/java/com/example/enf_cnam/ExamenActivity.java
+++ app/src/main/java/com/example/enf_cnam/ExamenActivity.java
@@ -6,6 +6,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ExamenActivity extends AppCompatActivity {
 
@@ -13,6 +25,52 @@ public class ExamenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_examen);
+        Thread exams = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONArray examens = listExamens(MainActivity.formation.getInt("ID_FORMATION"));
+                    LinearLayout unExamen = new LinearLayout(getApplicationContext());
+                    unExamen.setOrientation(LinearLayout.VERTICAL);
+                    if (examens != null) {
+                        for(int i = 0; i < examens.length(); i++) {
+                            JSONObject examen = examens.getJSONObject(i);
+                            TextView horaires = new TextView(getApplicationContext());
+                            horaires.setText(examen.getString("HEURE_DEB") + " à " + examen.getString("HEURE_FIN"));
+                            unExamen.addView(horaires);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        exams.start();
+    }
+
+    /**
+     * Fonction: listExamens
+     * Description: Liste les examens retournés depuis l'API
+     * @param idFormation Identifiant de la formation
+     * @return JSONArray
+     */
+    public JSONArray listExamens(int idFormation) {
+        JSONArray results = null;
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("http://apicnam.000webhostapp.com/API/Controllers/PlanningController.php?view=getExamens&id=" + idFormation)
+                    .build();
+            Response reponse = client.newCall(request).execute();
+            String responseBody = reponse.body().string();
+            JSONObject jsonReponse = new JSONObject(responseBody);
+            if (jsonReponse.has("examens")) {
+                results = jsonReponse.getJSONArray("examens");
+            }
+        } catch(IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return results;
     }
 
     public void viewHome(View v) {
